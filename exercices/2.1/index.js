ef0ca78ee8a6427f0f77bd3993859ee4df8ccab6
Arthi25 <arthur.stoffels25@gmail.com>
2023-10-09 14:54:30 +0200

    correction ex 2.1

--- exercices/2.1/index.js
+++ exercices/2.1/index.js
@@ -1,11 +1,11 @@
-const DEFAULT_MESSAGE = 
-    'this is the best moment to have a look at this website !';
+const DEFAULT_MESSAGE =
+  'This is the best moment to have a look at this website !';
 const fullMessage = addDateTime(DEFAULT_MESSAGE);
 alert(fullMessage);
 
 function addDateTime(message) {
-    const dateTimeNow = new Date();
-    const date = dateTimeNow.toLocaleDateString();
-    const hou = dateTimeNow.toLocaleTimeString([], {timeStyle: 'short'});
-    return date + ' ' + hour + ' : ' + message;
+  const dateTimeNow = new Date();
+  const date = dateTimeNow.toLocaleDateString(); // 17/08/2020
+  const hour = dateTimeNow.toLocaleTimeString([], { timeStyle: 'short' }); // 13:26:15
+  return date + ' ' + hour + ' : ' + message;
 }
