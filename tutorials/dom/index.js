60d8d3cbc2e9e6709300c446eab28a616075266c
Arthi25 <arthur.stoffels25@gmail.com>
2023-10-09 15:31:46 +0200

    advancing in the tutorials

---
+++ tutorials/dom/index.js
@@ -0,0 +1,7 @@
+let btn1 = document.getElementById("myBtn1");
+
+let btn2 = document.querySelector("#myBtn2"); // HTML id attribute
+let msg = document.querySelector(".message"); // CSS class name
+let duplicateMsg = document.querySelector("div"); // HTML tag name
+
+btn2.innerText = "I have been clicked";
